bdf7ed0a722582ed33643bda708dfa9ee7f62ac6
kulagina <olga.kulagina17@yandex.ru>
2024-06-06 17:06:49 +0300

    add Sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -7,7 +7,10 @@ const AppRouter = () => {
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         {Object.values(routeConfig).map(({ path, element }) => (
-          <Route path={path} element={element}></Route>
+          <Route
+            path={path}
+            element={<div className="page-wrapper">{element}</div>}
+          />
         ))}
       </Routes>
     </Suspense>
