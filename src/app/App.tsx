6b6f8b98f387928c2e11b5ce0f195fabde04b5bc
kulagina <olga.kulagina17@yandex.ru>
2024-06-12 12:13:26 +0300

    add eslint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,28 @@
-import { classNames } from "shared/lib/classNames/classNames";
-import { Navbar } from "widgets/Navbar";
-import { Sidebar } from "widgets/Sidebar";
+import { Suspense } from 'react';
 
-import { useTheme } from "./providers/ThemeProvider";
-import { AppRouter } from "./providers/router";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
-import "./styles/index.scss";
-import { Suspense } from "react";
+import { useTheme } from './providers/ThemeProvider';
+import { AppRouter } from './providers/router';
+
+import './styles/index.scss';
 
 const App = () => {
-  const { theme } = useTheme();
+    const { theme } = useTheme();
 
-  return (
-    <div className={classNames("app", {}, [theme])}>
-      <Suspense fallback="">
-        <Navbar />
-        <div className="content-page">
-          <Sidebar />
-          <AppRouter />
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
-      </Suspense>
-    </div>
-  );
+    );
 };
 
 export default App;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,31 +1,31 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
-import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { classNames } from 'shared/lib/classNames/classNames';
 
-import cls from "./Navbar.module.scss";
+import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  return (
-    <div className={classNames(cls.navbar, {}, [className])}>
-      <div className={cls.links}>
-        <AppLink
-          to={"/"}
-          className={cls.mainLink}
-          theme={AppLinkTheme.SECONDARY}
-        >
-          {t("Главная")}
-        </AppLink>
-        <AppLink to={"/about"} theme={AppLinkTheme.SECONDARY}>
-          {t("О сайте")}
-        </AppLink>
-      </div>
-    </div>
-  );
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink
+                    to="/"
+                    className={cls.mainLink}
+                    theme={AppLinkTheme.SECONDARY}
+                >
+                    {t('Главная')}
+                </AppLink>
+                <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
+                    {t('О сайте')}
+                </AppLink>
+            </div>
+        </div>
+    );
 };
