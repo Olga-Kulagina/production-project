ddae2a020b4f7925b016d5e76df484103c49d6c9
kulagina <olga.kulagina17@yandex.ru>
2024-06-12 19:40:01 +0300

    add NotFoundPage and Loader

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,20 @@
+import './Loader.scss';
+
+export const Loader = () => (
+    <div className="loadingio-spinner-spinner-nq4q5u6dq7r">
+        <div className="ldio-x2uulkbinbj">
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+        </div>
+    </div>
+);
