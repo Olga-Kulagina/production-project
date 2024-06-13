b9756c90400dbcbdc3d43c1f65a4bd1af79e5e78
kulagina <olga.kulagina17@yandex.ru>
2024-06-13 12:30:44 +0300

    add ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import { Button } from 'shared/ui/Button/Button';
+
+// Компонент для тестирования ErrorBoundary
+export const BugButton = () => {
+    const [error, setError] = useState(false);
+
+    const onThrow = () => setError(true);
+
+    useEffect(() => {
+        if (error === true) {
+            throw new Error();
+        }
+    }, [error]);
+
+    return (
+        // eslint-disable-next-line i18next/no-literal-string
+        <Button onClick={onThrow}>
+            throw Error
+        </Button>
+    );
+};
