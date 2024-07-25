10abff457a8a30c87c9ce9e725219eb432be37fa
kulagina <olga.kulagina17@yandex.ru>
2024-07-25 21:44:25 +0300

    refactoring babelLoader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,22 +1,14 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     };
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-            },
-        },
-    };
+    const babelLoader = buildBabelLoader();
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif)$/i,
